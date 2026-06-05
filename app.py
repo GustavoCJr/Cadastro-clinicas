0b214dd48d58094819293c93c6c1fd1e0d559bb4
Gustavo Cassettari <cassettarigustavo476@gmail.com>
2026-06-05 20:30:59 -0300

    feat: adicionar rotas com integracao a API do ViaCEP

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+import requests
+from services.aqui import (
+    salvar_banco,
+    ler_banco,
+    validar_cep
+)
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/cadastrar-clinica", methods=['POST'])
+def cadastrar_clinica():
+    dados_clinica = request.form.to_dict()
+    #fazer validações
+    dados_antigos = ler_banco()
+    #Fazer tratamento de erros
+    dados_antigos.append(dados_clinica)
+    salvar_banco(dados_antigos)
+    return "Clinica cadastrada com sucesso!"
+
+@app.route("/buscar_cep", methods=['POST'])
+def buscar_cep():
+    dados = request.get_json()
+    cep = dados.get('cep')
+    cep_sem_masc = cep.replace("-", "")
+    erro = validar_cep(cep_sem_masc)
+    if erro["erro"] == True:
+        return erro["mensagem"]
+    else:
+        resposta = requests.get(
+            f"https://viacep.com.br/ws/{cep_sem_masc}/json/"
+        )
+        dados_endereco = resposta.json()
+        if "erro" in dados_endereco:
+            return jsonify({"erro": "CEP inexistente"}), 404
+        return jsonify(dados_endereco)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
